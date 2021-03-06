28cf0ff1e6980aa3404c3f2a10e0cf8cd5edb69c
Becan <55333622+Becan@users.noreply.github.com>
2021-03-06 17:05:36 +0100

    Cowboy Walk in + Shoot
    Added cowboy walk in animation, shooting rotation and shooting animation

---
+++ GameJam/Assets/Scripts/CowboyController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class CowboyController : MonoBehaviour
+{
+    private void Update()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.z = transform.position.z;
+
+        Quaternion lookRotation = Quaternion.LookRotation(mousePosition - transform.position);
+        lookRotation = Quaternion.Euler(0.0f, 0.0f, -lookRotation.eulerAngles.x);
+        transform.rotation = lookRotation;
+    }
+}
---
+++ GameJam/Assets/Scripts/Testic.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Testic : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+
+            GetComponent<Animator>().Play("Arm_shoot", 0, 0.0f);
+    }
+}
