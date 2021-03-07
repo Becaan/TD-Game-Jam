dbb7d52d88b3a75865adf3bfd284f2ddf96155a9
Becan <55333622+Becan@users.noreply.github.com>
2021-03-07 01:10:43 +0100

    Added bandits and bullet trails

---
+++ GameJam/Assets/Scripts/Bandit.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bandit : MonoBehaviour
+{
+    Rigidbody2D myRigidbody2D;
+
+    private void Start()
+    {
+        myRigidbody2D = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Trigger");
+        if (collision.CompareTag("Bullet"))
+        {
+            //myRigidbody2D.isKinematic = false;
+            Destroy(gameObject);
+        }
+    }
+    
+}
--- GameJam/Assets/Scripts/Bullet.cs
+++ GameJam/Assets/Scripts/Bullet.cs
@@ -5,12 +5,27 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     [SerializeField] float speed = 20f;
+    [SerializeField] Vector2 widthThresold;
+    [SerializeField] Vector2 heightThresold;
     Rigidbody2D myRigidbody2D;
 
-    void Start()
+    private void Start()
     {
         myRigidbody2D = transform.GetComponent<Rigidbody2D>();
         myRigidbody2D.velocity = transform.right * speed;
+    }
+
+    void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Ground"))
+        {
+            Destroy(gameObject);
+        }
     }
 
 }
